1ae849768b283e846a6b0f97ebcf9cc786696ad2
SOAM\1758828 <cardona.james@tcs.com>
2020-11-10 02:48:34 -0500

    creacion del taller 1 del curso de java basico

---
+++ src/clases/Constantes.java
@@ -0,0 +1,10 @@
+package clases;
+
+public class Constantes {
+	public static final String TEXTO_INGRESOS = "Ingrese sus ingresos mensuales";
+	public static final String TEXTO_GASTOS_FIJOS = "Ingrese sus gastos Fijos";
+	public static final String TEXTO_GASTOS_VARIABLES = "Ingrese sus gastos Variables";
+	public static final String TEXTO_CAPACIDAD_ENDEUDAMIENTO = "Su capacidad de endeudamiento es: ";
+	public static final String TEXTO_MENSAJE_ERROR_NUMERICO = "Debe ingresar un valor numerico";
+	public static final double INTERES_VENCIDO = 2.13;
+}
